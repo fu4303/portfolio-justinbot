23fe357c44e8774b3b5132270386df9fb1a5ae47
Justin Carlson <jrcarlson8@gmail.com>
2020-09-26 15:31:11 -0400

    Add Bulma built with SASS.

--- .eleventy.js
+++ .eleventy.js
@@ -7,10 +7,21 @@ module.exports = (config) => {
   // Support YAML data
   config.addDataExtension('yml', (contents) => yaml.safeLoad(contents));
 
+  // Use these template formats
+  config.setTemplateFormats(['njk']);
+
+  // Pass-through files
+  config.addPassthroughCopy('src/robots.txt');
+  config.addPassthroughCopy('src/favicon.ico');
+  config.addPassthroughCopy('src/assets/images');
+
+  // TOOD Minify in production
+
   return {
     dir: {
       input: 'src',
       output: 'dist',
     },
+    htmlTemplateEngine: 'njk',
   };
 };
