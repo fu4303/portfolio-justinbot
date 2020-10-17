7ac2fb284176b3a01baa6eb36ec99f24f3d18d98
Justin Carlson <jrcarlson8@gmail.com>
2020-10-16 23:36:38 -0400

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import '../css/index.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
+// TODO Import only used icons to reduce bundle size.
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 
-console.log('%cTaking a look under the hood? Check out the source at https://github.com/justinbot/justincarlson-io', 'color:blue');
+console.log('%c>>> Taking a look under the hood? See the source code at https://github.com/justinbot/justincarlson-io', 'background: #121212; color: #e0ff8c');
